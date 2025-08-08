1506fbff64081c0a0c3660e387a5efaa9b044c57
Rafakka <rafaelkaher@gmail.com>
2025-08-08 08:10:34 -0300

    atualizando comandos

--- src/main/java/com/logos/gutensearch/repository/LivroRepository.java
+++ src/main/java/com/logos/gutensearch/repository/LivroRepository.java
@@ -4,11 +4,24 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.logos.gutensearch.model.Livro;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+    
     Optional<Livro> findByTitulo(String titulo);
+
     List<Livro> findByIdioma(String idioma);
+
     Object estatisticas();
+
+    @Query("SELECT DISTINCT l.idioma FROM Livro l")
+    List<String> findDistinctIdiomas();
+
+    @Query("SELECT l FROM Livro l WHERE l.autor.id = ?1")
+    List<Livro> findByAutorId(Long autorId);
+
+    @Query("SELECT l FROM Livro l WHERE l.genero = ?1")
+    List<Livro> findByGenero(String genero);
 }
